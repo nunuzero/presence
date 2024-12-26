a7df6c67854569388c9bfb33b3c5dcf9f98109f3
Anugerah Ashar <nunuzero10@gmail.com>
2024-12-26 09:52:46 +0800

    add staff management feature

---
+++ app/Models/Staff.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Staff extends Model
+{
+    protected $fillable = [
+        'name',
+        'start_date',
+        'education',
+        'note',
+        'user_id',
+        'position_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function position(): BelongsTo
+    {
+        return $this->belongsTo(Position::class);
+    }
+}
