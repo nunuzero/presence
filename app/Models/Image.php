04db31da849f67a1bd2e179e67cc32aaeac5ced7
Anugerah Ashar <nunuzero10@gmail.com>
2025-01-31 21:10:56 +0800

    add report for staff

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -14,16 +14,16 @@ class Image extends Model
 
     protected static function booted(): void
     {
-        static::updating(function ($user) {
-            $originalImage = $user->getOriginal('logo');
-            $originalImage2 = $user->getOriginal('login_background');
-            
-            if ($originalImage && Storage::disk('logo')->exists($originalImage)) {
-                Storage::disk('logo')->delete($originalImage);
+        static::updating(function ($image) {
+            $originalLogo = $image->getOriginal('logo');
+            $originalLoginBackground = $image->getOriginal('login_background');
+    
+            if ($originalLogo && $image->isDirty('logo') && Storage::disk('public')->exists($originalLogo)) {
+                Storage::disk('public')->delete($originalLogo);
             }
-
-            if ($originalImage2 && Storage::disk('login_background')->exists($originalImage2)) {
-                Storage::disk('login_background')->delete($originalImage2);
+    
+            if ($originalLoginBackground && $image->isDirty('login_background') && Storage::disk('public')->exists($originalLoginBackground)) {
+                Storage::disk('public')->delete($originalLoginBackground);
             }
         });
     }
